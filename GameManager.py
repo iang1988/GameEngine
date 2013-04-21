14817768065036837fa53811c445e4fff25d0c92
stealthycoin <john.w.carlyle@gmail.com>
2013-04-20 17:20:36 -0700

    Added example of how to use a game action

--- GameManager.py
+++ GameManager.py
@@ -1,13 +1,29 @@
 import pygame
+import GameAction as ga
 import World
 
 class GameManager :
-    def __init__(self,size):
+    def __init__(self, size, im):
         self.size=size
         self.x=0
         self.W=World.World(size)
+        self.im = im
+        
+        #input testing
+        self.paused = False
+        self.pause = ga.GameAction("Pause", ga.GameAction.DETECT_INITIAL_PRESS_ONLY) #create action
+        self.im.mapToKey(self.pause, pygame.K_SPACE) #register it with the input manager for the space key
+        
+
     def update(self,dtime):
-        self.W.update(dtime)
+        #check if the action was triggered
+        if self.pause.getAmount() > 0:
+            self.paused = not self.paused
+
+        if not self.paused:
+            self.W.update(dtime)
+
     def draw(self,screen):
-        pygame.draw.rect(screen,[0,0,0],[0,0,self.size[0],self.size[1]])
-        self.W.draw(screen)
+        if not self.paused:
+            pygame.draw.rect(screen,[0,0,0],[0,0,self.size[0],self.size[1]])
+            self.W.draw(screen)

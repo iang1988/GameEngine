d20de67749422c5ac50272aff2f6bfa517b96ebe
stealthycoin <john.w.carlyle@gmail.com>
2013-04-29 01:07:02 -0700

    Added acceleration/velocity/position vectors and adjusted so that velocity is in pixels per second

--- Sprite.py
+++ Sprite.py
@@ -1,25 +1,35 @@
 import pygame
 import random as r
 import Animation
+import numpy as np #now using numpy library
 
 class Sprite:
     def __init__(self,ioa,size):
         if isinstance(ioa, Animation.Animation): #if it is an animation, load it as such
             self.anim=ioa
-        else:#otehrwise, it must be an image, make an animation with 1 frame and put the image in that.
+        else:#otherwise, it must be an image, make an animation with 1 frame and put the image in that.
             self.anim=Animation.Animation()
             self.anim.addFrame(ioa,10)
 
-        self.x=r.gauss(size[0]/2,80)
-        self.y=r.gauss(size[1]/2,80)
-
+        #acceleration vector
+        self.acceleration = np.array([0.0,0.0])
+        #velocity vector
+        self.velocity = np.array([0.0,0.0])
+        #position vector
+        self.position = np.array([r.gauss(size[0]/2,80),
+                                  r.gauss(size[1]/2,80)])
+        
+        
     def update(self,dtime):
         self.anim.update(dtime)
+
+        dtimeSeconds = dtime / 1000.0
+        self.velocity += self.acceleration * dtimeSeconds
+        self.position += self.velocity * dtimeSeconds
         
     def draw(self,screen):
-        #pygame.draw.rect(screen,self.c,[self.x,self.y,self.w,self.h])
-        screen.blit(self.anim.getFrame().img,(self.x,self.y))
-        #()tuple a list that can't be modified
+        screen.blit(self.anim.getFrame().img,self.position.tolist())
+        #() tuple a list that can't be modified
 
     
         
